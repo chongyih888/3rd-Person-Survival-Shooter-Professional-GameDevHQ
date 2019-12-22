06ff6fd2cabc0b8a14dfedd4f122e3e7f849dc64
Lim Chong Yih <chongyih21@gmail.com>
2019-12-22 10:36:39 +0800

    Created player and shooting

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //get handle to character controller
+    private CharacterController _controller;
+
+    [Header("Controller Settings")]
+    [SerializeField]
+    private float _speed = 6.0f;
+
+    [SerializeField]
+    private float _jumpHeight = 8.0f;
+
+    [SerializeField]
+    private float _gravity = 20.0f;
+
+    private Vector3 _velocity;
+
+    private Vector3 direction;
+
+    private Camera _mainCamera;
+
+    [Header("Camera Settings")]
+    [SerializeField]
+    private float _cameraSensitivity = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+
+        if(_controller == null)
+        {
+            Debug.Log("Character controller is NULL");
+        }
+
+        _mainCamera = Camera.main;
+
+        if (_mainCamera == null)
+        {
+            Debug.LogError("Main Camera is NULL");
+        }
+
+        //lock cursor and hide it
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        CameraController();
+
+        //if escapekey
+        //unlock cursor
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+         }
+
+    void CalculateMovement()
+    {
+        //wsad keys for movement
+        //input system(horizontal,vertical)
+        //direction = vector to move 
+        //velocity = direction * speed
+        //if jump
+        //velocity = new velocity with added y
+        if (_controller.isGrounded == true)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            direction = new Vector3(horizontalInput, verticalInput, 0);
+            _velocity = direction * _speed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _velocity.y = _jumpHeight;
+            }
+        }
+       
+
+            _velocity.y -= _gravity * Time.deltaTime;
+        
+           _velocity = transform.TransformDirection(_velocity);
+
+        //controller.move(wasd * Time.delta);
+        _controller.Move(_velocity * Time.deltaTime);
+
+    }
+
+    void CameraController()
+    {
+        //x mouse
+        //y mouse
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        //apply mouseX to the player rotation y to allow us to move left and right
+        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + mouseX, transform.localEulerAngles.z);
+
+        Vector3 currentRotation = transform.localEulerAngles;
+        currentRotation.y += mouseX * _cameraSensitivity;
+        transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
+
+        //apply mouseY to the camera X value and clamp between 0 and 15
+        Vector3 currentCameraRotation = _mainCamera.gameObject.transform.localEulerAngles;
+        currentCameraRotation.x -= mouseY * _cameraSensitivity;
+        currentCameraRotation.x = Mathf.Clamp(currentCameraRotation.x, 0, 26);
+
+        // _mainCamera.gameObject.transform.localEulerAngles = currentCameraRotation;
+        _mainCamera.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCameraRotation.x, Vector3.right);
+
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootRay();
+    }
+
+    void ShootRay()
+    {
+        //left click to fire
+        //cast a ray from the center of the screen of the screen
+        //debug the name of the object you hit
+        if (Input.GetMouseButtonDown(0))
+        {
+            //starting point
+            //hitInfo
+            Vector3 center = new Vector3(0.5f, 0.5f, 0);
+            Ray rayOrigin = Camera.main.ViewportPointToRay(center);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log("Hit: " + hitInfo.collider.name);
+            }
+        }
+    }
+}
